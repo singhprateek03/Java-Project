80123144bc863d1f80b3a234ecf2de62f7c88863
Prateek Singh <prateeksingh3102000@gmail.com>
2024-09-18 00:01:12 +0530

    Added comments

--- src/Board.java
+++ src/Board.java
@@ -5,30 +5,30 @@ import java.awt.event.*;
 
 public class Board extends JPanel implements ActionListener {
 
-    private Image apple;
-    private Image dot;
-    private Image head;
+    private Image apple;    // Image AWT ki class hai
+    private Image dot;      // Image AWT ki class hai
+    private Image head;     // Image AWT ki class hai
 
-    private final int ALL_DOTS = 900;
-    private final int DOT_SIZE = 10;
-    private final int RANDOM_POSITION = 29;
+    private final int ALL_DOTS = 900;      // All_DOTS frame ka size hai
+    private final int DOT_SIZE = 10;       // yeah 1 dot ka size hai
+    private final int RANDOM_POSITION = 29; // yeah randomly kahi bhi apple de sakta hai frame ke andar
 
     private int apple_x;
     private int apple_y;
 
-    private final int x[] = new int[ALL_DOTS];
+    private final int x[] = new int[ALL_DOTS];  // snake apples khane ke baad jo badhta jayega woo iss main store hoga
     private final int y[] = new int[ALL_DOTS];
 
     private boolean leftDirection = false;
-    private boolean rightDirection = true;
+    private boolean rightDirection = true;      // by default right main hee chale isi liye true kiya hai
     private boolean upDirection = false;
     private boolean downDirection = false;
 
     private boolean inGame = true;
-
+    // Global variable
     private int dots;
     private Timer timer;
-
+    // Default constructor
     Board() {
         addKeyListener(new TAdapter());
 
@@ -36,13 +36,14 @@ public class Board extends JPanel implements ActionListener {
         setPreferredSize(new Dimension(300, 300));
         setFocusable(true);
 
-        loadImages();
-        initGame();
+        loadImages();   // method call
+        initGame();     // method call
     }
 
     public void loadImages() {
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons\\apple.png"));
-        apple = i1.getImage();
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons\\apple.png"));  // getSystemResource() yeah ek static function hai jo ki ClassLoader ke andar hota hai
+
+        apple = i1.getImage();  // yaha image ko store kar rahe hai
 
         ImageIcon i2 = new ImageIcon(ClassLoader.getSystemResource("icons\\dot.png"));
         dot = i2.getImage();
@@ -55,28 +56,30 @@ public class Board extends JPanel implements ActionListener {
         dots = 3;
 
         for (int i = 0; i < dots; i++) {
-            y[i] = 50;
+            y[i] = 50;   // frame ke andar upar se 50 ka gap aayega
             x[i] = 50 - i * DOT_SIZE;
         }
 
-        locateApple();
+        locateApple(); // method call
 
         timer = new Timer(140, this);
         timer.start();
     }
 
     public void locateApple() {
-        int r = (int)(Math.random() * RANDOM_POSITION);
-        apple_x = r * DOT_SIZE;
+        // yeah x ke liye hai
+        int r = (int)(Math.random() * RANDOM_POSITION); // Type Casting agar nhi karenege toh value float main aayegi
+        apple_x = r * DOT_SIZE;  // r main random number aayega or DOT_SIZE apple ka size batayega
 
+        // yeah y ke liye hai
         r = (int)(Math.random() * RANDOM_POSITION);
-        apple_y = r * DOT_SIZE;
+        apple_y = r * DOT_SIZE; // r main random number aayega or DOT_SIZE apple ka size batayega
     }
 
     public void paintComponent(Graphics g) {
-        super.paintComponent(g);
+        super.paintComponent(g);    // super ki help se hum parent ke component ko call karta hai
 
-        draw(g);
+        draw(g);     // method call
     }
 
     public void draw(Graphics g) {
@@ -91,9 +94,9 @@ public class Board extends JPanel implements ActionListener {
                 }
             }
 
-            Toolkit.getDefaultToolkit().sync();
+            Toolkit.getDefaultToolkit().sync();     // Default se initialized ho jayega
         } else {
-            gameOver(g);
+            gameOver(g);    // method call
         }
     }
 
@@ -113,16 +116,16 @@ public class Board extends JPanel implements ActionListener {
             y[i] = y[i - 1];
         }
 
-        if (leftDirection) {
+        if (leftDirection) {      // isse snake right side ko jayega
             x[0] = x[0] - DOT_SIZE;
         }
-        if (rightDirection) {
+        if (rightDirection) {   // isse snake left side ko jayega
             x[0] = x[0] + DOT_SIZE;
         }
-        if (upDirection) {
+        if (upDirection) {       // isse snake down side ko jayega
             y[0] = y[0] - DOT_SIZE;
         }
-        if (downDirection) {
+        if (downDirection) {      // isse snake up side ko jayega
             y[0] = y[0] + DOT_SIZE;
         }
     }
@@ -130,17 +133,19 @@ public class Board extends JPanel implements ActionListener {
     public void checkApple() {
         if ((x[0] == apple_x) && (y[0] == apple_y)) {
             dots++;
-            locateApple();
+            locateApple();  // method call
         }
     }
 
     public void checkCollision() {
+        // snake agar khud ko kaat leta hai toh collision hoga
         for(int i = dots; i > 0; i--) {
             if (( i > 4) && (x[0] == x[i]) && (y[0] == y[i])) {
                 inGame = false;
             }
         }
 
+        // snake agar boundry se takrata hai toh collision hoga
         if (y[0] >= 300) {
             inGame = false;
         }
@@ -159,40 +164,42 @@ public class Board extends JPanel implements ActionListener {
         }
     }
 
-    public void actionPerformed(ActionEvent ae) {
+    public void actionPerformed(ActionEvent ae) {   // yeah method ko override kar dega
         if (inGame) {
-            checkApple();
-            checkCollision();
-            move();
+            checkApple();   // method call
+            checkCollision();   // method call
+            move(); // method call
         }
 
-        repaint();
+        repaint();    // method call
     }
 
-    public class TAdapter extends KeyAdapter {
+    public class TAdapter extends KeyAdapter {      // isse humare keys work karegi
         @Override
         public void keyPressed(KeyEvent e) {
             int key = e.getKeyCode();
 
-            if (key == KeyEvent.VK_LEFT && (!rightDirection)) {
+            if (key == KeyEvent.VK_LEFT && (!rightDirection)) {     // VK_LEFT -> left wali key ka sara kaam dekehgi
                 leftDirection = true;
+            //  rightDirection -> yeah false nhi karenge kyu ki agar false kar diya toh user left to right switch kar jayega jo ki nhi hona chaiye
                 upDirection = false;
                 downDirection = false;
             }
 
-            if (key == KeyEvent.VK_RIGHT && (!leftDirection)) {
+            if (key == KeyEvent.VK_RIGHT && (!leftDirection)) { // VK_LEFT -> left wali key ka sara kaam dekehgi
                 rightDirection = true;
+            //  leftDirection -> yeah false nhi karenge kyu ki agar false kar diya toh user left to right switch kar jayega jo ki nhi hona chaiye
                 upDirection = false;
                 downDirection = false;
             }
 
-            if (key == KeyEvent.VK_UP && (!downDirection)) {
+            if (key == KeyEvent.VK_UP && (!downDirection)) {     // VK_LEFT -> left wali key ka sara kaam dekehgi
                 upDirection = true;
                 leftDirection = false;
                 rightDirection = false;
             }
 
-            if (key == KeyEvent.VK_DOWN && (!upDirection)) {
+            if (key == KeyEvent.VK_DOWN && (!upDirection)) {    // VK_LEFT -> left wali key ka sara kaam dekehgi
                 downDirection = true;
                 leftDirection = false;
                 rightDirection = false;
